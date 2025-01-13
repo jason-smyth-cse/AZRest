34be78d072ca6e8fb0363b9ebcad312e60fb4e83
Jason Smyth <165066158+jason-smyth-cse@users.noreply.github.com>
2025-01-13 14:24:03 +0000

    Updating ID value of Azure Resource object
    Processing passed [psobject] Azure Resource and updating its ID property with placeholder values representing the Subscription ID, Resource Group Name and Workspace Name.

--- public/Set-AzureObject.ps1
+++ public/Set-AzureObject.ps1
@@ -1,10 +1,14 @@
 function Set-AzureObject(){
 
 param(
-    [parameter( Mandatory = $false)]
+    [parameter(Mandatory = $false)]
     [string]$Subscription,
-    [parameter( Mandatory = $true)]
-    [hashtable]$AzObject
+	[parameter(Mandatory = $false)]
+    [string]$ResourceGroup,
+	[parameter(Mandatory = $false)]
+    [string]$Workspace,
+    [parameter(Mandatory = $true)]
+	[psobject]$AzObject
 )
 
 <#
@@ -15,22 +19,34 @@ param(
             Typically this might be changing subscription or resourcegroup values
             for testing.
 
-  Parameters:   -object        = The PowerShell custom object / Azure object to be modified
+  Parameters:   -object        = The PowerShell custom object / Azure object to be modified (#[hashtable]$AzObject)
                 -subscription  = The new subscription gui to deploy to
+				-resourcegroup = The new resource group gui to deploy to
+				-workspace 	   = The new workspace gui to deploy to
 
 
   Example:  
     
-          $object = Set-AzureObject -object $object -Subscription "2be53ae5-6e46-47df-beb9-6f3a795387b8"
-#> 
+          $objectIDupdate = Set-AzureObject -AzObject $object -Subscription "2be53ae5-6e46-47df-beb9-6f3a795387b8" -ResourceGroup "rg-sentinel-v2" -Workspace "ingested-data-sentinel-v2"		  
+#>
 
+Process  {
+    if ($Subscription -ne $null){  
+      $IdString = Set-IDSubscription -IdString $AzObject.id -Subscription $Subscription 
+      $AzObject.id = $IdString
+    }
 
-    if ($Subscription){  
-      $IdString = Set-IdSubscription -IdString $AzObject.id -Subscription $Subscription 
+	if ($ResourceGroup -ne $null){  
+      $IdString = Set-IDResourceGroup -IdString $AzObject.id -ResourceGroup $ResourceGroup 
+      $AzObject.id = $IdString
+    }
+
+    if ($Workspace -ne $null){  
+      $IdString = Set-IDWorkspace -IdString $AzObject.id -Workspace $Workspace 
       $AzObject.id = $IdString
     }
 
     #return the object
-     $AzureObject
-
+	return $AzObject
+    }
 }
